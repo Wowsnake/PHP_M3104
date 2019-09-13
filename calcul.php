3739ec2852b0d13ddb1cad2f81fbdd0ffe63ebe2
a18006967 <steffen.alvarez@etu.univ-amu.fr>
2019-09-13 11:32:30 +0200

    exo 5.2

--- calcul.php
+++ calcul.php
@@ -16,15 +16,6 @@
         {
             echo'<br/><strong>opérateur'.$op.'nongéré</strong>';
         }
-    $operateurs = '*+-/';
-    for($cpt=0;$cpt<=3;++$cpt)
-    {
-        echo'<input';
-        if($cpt==0)
-        {
-            echo'checked="checked"';
-        }
-        echo'type="radio"name="op"value="'.$operateurs[$cpt].'"/>'.$operateurs[$cpt].'<br/>
-        '."\n";}
+
     end_page();
     ?>
--- calculator.php
+++ calculator.php
@@ -1,13 +1,20 @@
 <?php
     include 'index.php';
     start_page("calculatrice");
+    $operateurs = '*+-/';
     echo '
     <form action = "calcul.php" method = "post"
     <input type = "text" name = "op1" >
-    <input type = "text" name = "op2" >
-    <input checked="checked" type="radio" name="op" value="*"/>*<br/>
-    <input type="radio" name="op" value="+"/>+<br/>
-    <input type="radio" name="op" value="-"/>-<br/>
-    <input type="radio" name="op" value="/"/>/<br/></form>';
+    <input type = "text" name = "op2" >';
+    for($cpt=0;$cpt<=3;++$cpt)
+    {
+        echo'<input';
+        if($cpt==0)
+        {
+            echo'checked="checked"';
+        }
+        echo'type="radio"name="op"value="'.$operateurs[$cpt].'"/>'.$operateurs[$cpt].'<br/>
+        '."\n";}
+    echo '</form>';
     end_page();
     ?>

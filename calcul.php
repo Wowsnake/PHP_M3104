050b4486e2be1e8a6375c23294251b1a6af37dc8
a18006967 <steffen.alvarez@etu.univ-amu.fr>
2019-09-13 11:27:59 +0200

    exo 5.2

--- calcul.php
+++ calcul.php
@@ -16,4 +16,17 @@
         {
             echo'<br/><strong>opérateur'.$op.'nongéré</strong>';
         }
+    $operateurs = '*+-/';
+    $operateurs[0];
+    $operateurs[1];
+    $operateurs[2];
+    for($cpt=0;$cpt<=3;++$cpt)
+    {
+        echo'<input';
+        if($cpt==0)
+        {
+            echo'checked="checked"';
+        }
+        echo'type="radio"name="op"value="'.$operateurs[$cpt].'"/>'.$operateurs[$cpt].'<br/>
+        '."\n";}
     end_page();

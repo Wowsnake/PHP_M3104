f52ff5f1974eeb0804cc95912ea7d8a68840bf8b
a18006967 <steffen.alvarez@etu.univ-amu.fr>
2019-09-13 11:53:36 +0200

    fin

--- calcul.php
+++ calcul.php
@@ -1,16 +1,17 @@
 <?php
     include "index.php";
     start_page("calcul");
-    $op1 = $_POST['op1'];
-    $op2 = $_POST['op2'];
-    $op = $_POST['op'];
+    $op1 = $_GET['op1'];
+    $op2 = $_GET['op2'];
+    $op = $_GET['op'];
+    $action = $_GET['action'];
     if('*'== $op)
     {
-
+        $res = $op1 * $op2;
     }
     elseif('+'== $op)
     {
-
+        $res = $op1 + $op2;
     }
     else
         {
--- calculator.php
+++ calculator.php
@@ -4,7 +4,7 @@
     start_page("calculatrice");
     $operateurs = '*+-/';
     echo '
-    <form action = "calcul.php" method = "post">
+    <form action = "calcul.php" method = "get">
     <input type = "text" name = "op1" ><br>
     <input type = "text" name = "op2" ><br>';
     for($cpt=0;$cpt<=3;++$cpt)
@@ -14,8 +14,9 @@
         {
             echo'checked="checked"';
         }
-        echo'type="radio"name="op"value="'.$operateurs[$cpt].'"/>'.$operateurs[$cpt].'<br/>
+        echo'type="submit" name="op" value="'.$operateurs[$cpt].'"/>'.$operateurs[$cpt].'<br/>
         '."\n";}
     echo '</form>';
+
     end_page();
     ?>

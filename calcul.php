9b3c2d8da396bcee54998820f8d56360e5015756
a18006967 <steffen.alvarez@etu.univ-amu.fr>
2019-09-13 11:22:58 +0200

    exo 5.2

--- calcul.php
+++ calcul.php
@@ -4,4 +4,16 @@
     $op1 = $_POST['op1'];
     $op2 = $_POST['op2'];
     $op = $_POST['op'];
+    if('*'== $op)
+    {
+
+    }
+    elseif('+'== $op)
+    {
+
+    }
+    else
+        {
+            echo'<br/><strong>opérateur'.$op.'nongéré</strong>';
+        }
     end_page();
